9e4ec85817ddcd011b0e76d87abca1c11f5ec4b6
dariocubillos <dariocubillos1@gmail.com>
2019-08-21 17:06:12 -0500

    Update
    Firts

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ListayDatos2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.MainGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainGrid
+            // 
+            this.MainGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MainGrid.Location = new System.Drawing.Point(12, 38);
+            this.MainGrid.Name = "MainGrid";
+            this.MainGrid.Size = new System.Drawing.Size(445, 299);
+            this.MainGrid.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 403);
+            this.Controls.Add(this.MainGrid);
+            this.Name = "Form1";
+            this.Text = "Lista y Datos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MainGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView MainGrid;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ListayDatos2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+            string connstring = @"server=localhost;userid=root;password=;database=zapateria";
+
+            MySqlConnection conn = null;
+            DataTable dt = new DataTable();
+
+            try
+            {
+                conn = new MySqlConnection(connstring);
+                conn.Open();
+
+                string query = "SELECT * FROM zapatos;";
+                MySqlDataAdapter da = new MySqlDataAdapter(query, conn);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "zapatos");
+                dt = ds.Tables["zapatos"];
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: {0}", ex.ToString());
+            }
+
+
+
+            this.MainGrid.Visible = true;
+            this.MainGrid.AutoGenerateColumns = true;
+
+            //foreach (DataColumn ColumnItem in dt.Columns)
+            //{
+            //    this.MainGrid.Columns.Add(ColumnItem.ColumnName, ColumnItem.ColumnName);
+
+            //}
+
+            this.MainGrid.DataSource = dt;
+
+            
+
+
+            //this.MainGrid.Refresh();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
